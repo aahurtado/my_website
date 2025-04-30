0b409609ce1be2d591169af085942edc44882e9d
Aaron Hurtado <aaronhurtado95@gmail.com>
2025-04-29 17:40:49 -0700

    Added tilt-card effect to some images on the site

--- script.js
+++ script.js
@@ -72,6 +72,38 @@ function updateProgressBar() {
 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Check if device is not mobile (screen width > 767px)
+    if (window.innerWidth > 767) {
+        const containers = document.querySelectorAll('.tilt-card-container');
+        
+        containers.forEach(container => {
+            const card = container.querySelector('.tilt-card');
+            
+            container.addEventListener('mousemove', (e) => {
+                const rect = container.getBoundingClientRect();
+                const x = e.clientX - rect.left;
+                const y = e.clientY - rect.top;
+                
+                const centerX = rect.width / 2;
+                const centerY = rect.height / 2;
+                
+                const rotateX = -((y - centerY) / centerY) * 10;
+                const rotateY = ((x - centerX) / centerX) * 10;
+                
+                card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+            });
+            
+            container.addEventListener('mouseleave', () => {
+                card.style.transform = 'rotateX(0) rotateY(0)';
+            });
+        });
+    }
+});
+
+
+
+
 // Add event listeners
 window.addEventListener('scroll', () => {
     updateActiveSection();
